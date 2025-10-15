d7b289ad7003adf78a52be67accf978db23f7d26
Petar <PetarDZYtber@gmail.com>
2025-10-15 09:21:03 +0200

    kifh

--- 2. web shop/routes/cartRoute.js
+++ 2. web shop/routes/cartRoute.js
@@ -10,12 +10,29 @@ router.get('/', async (req, res) => {
     }
     const cartProductsQuery = `select p.name, c.quantity, p.price, p.id, p.image_url
                             from cart_items c
-                            left join products p on c.product_id = p.id
+                            join products p on c.product_id = p.id
                             where c.session_guid = ?`
     try {
         const [results, fields] = await db.execute(cartProductsQuery, [sessionId])
-        console.log(results)
-        res.render('cart', { title: 'Cart' })
+        let cartItems = []
+        results.forEach((row) => {
+            const item = cartItems.find((cartItem => cartItem.id === row.id))
+            if (item) {
+                item.quantity = item.quantity + Number(row.quantity)
+                item.productTotal = item.price * item.quantity
+            } else {
+                const newCartItem = {
+                    id: row.id,
+                    name: row.name,
+                    price: Number(row.price),
+                    quantity: Number(row.quantity),
+                    imageUrl: row.image_url,
+                    productTotal: row.price * row.quantity
+                }
+                cartItems.push(newCartItem)
+            }
+        })
+        res.render('cart', { title: 'Cart', cartItems: cartItems})
     } catch (error) {
         res.render('error', { title: 'Error' })
     }
